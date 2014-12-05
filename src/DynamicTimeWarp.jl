9c31f37616c1a7aec5029bc6febd1753244a1c77
Joseph Fowler <drjoefowler@gmail.com>
2014-12-04 17:56:55 -0700

    Initial commit with dtw working on a pair of sequences.
    Also some basic tests are included.

--- src/DynamicTimeWarp.jl
+++ src/DynamicTimeWarp.jl
@@ -2,4 +2,101 @@ module DynamicTimeWarp
 
 # package code goes here
 
+# Dynamic Time Warping with a user-specified distance function
+
+function dtw(seq1::Vector, seq2::Vector, distance::Function=Distance.square)
+    const m=length(seq1)
+    const n=length(seq2)
+
+    cost11 = distance(seq1[1], seq2[1])
+    cost = zeros(typeof(cost11), m, n)
+
+    # Initialize first column and first row
+    cost[1,1] = cost11
+    for r=2:m
+        cost[r,1] = cost[r-1,1] + distance(seq1[r], seq2[1])
+    end
+    for c=2:n
+        cost[1,c] = cost[1,c-1] + distance(seq1[1], seq2[c])
+    end
+
+    # Build cost matrix
+    for c=2:n
+        for r=2:m
+            best_neighbor_cost = min(cost[r-1,c], cost[r-1,c-1], cost[r,c-1])
+            cost[r,c] = best_neighbor_cost + distance(seq1[r], seq2[c])
+        end
+    end
+
+    # Reconstruct the path. This is done from end-to-beginning and then reversed
+    match1 = Int[m]
+    match2 = Int[n]
+    r,c = m,n
+    while r>1 && c>1
+        down,diag,left = cost[r-1,c], cost[r-1,c-1], cost[r,c-1]
+        if diag <= min(down,left)
+            r -= 1
+            c -= 1
+        elseif down<left
+            r -= 1
+        else
+            c -= 1
+        end
+        push!(match1, r)
+        push!(match2, c)
+    end
+    r = match1[end]
+    while r>1; r-=1; push!(match1, r); push!(match2, 1); end
+    c = match2[end]
+    while c>1; c-=1; push!(match1, 1); push!(match2, c); end 
+    reverse!(match1)
+    reverse!(match2)
+    
+    cost[m,n], match1, match2
+end
+
+
+using PyPlot
+
+function plotdtw{T<:Real}(seq1::Vector{T}, seq2::Vector{T}, offset::Float64=0.0)
+    cost, match1, match2 = dtw(seq1, seq2)
+    if offset == 0.0
+        offset = 2*(std(seq1) + std(seq2)) + mean(seq1) - mean(seq2)
+    end
+    clf()
+    plot(seq1, "-r", seq2+offset, "-b")
+    for i=1:length(match1)
+        plot([match1[i],match2[i]]-1, [seq1[match1[i]], seq2[match2[i]]+offset],
+             color="gray")
+    end
+end
+
+
+
+# This sub-module contains various functions returning pointwise distances
+# between elements from two seequences.
+
+module Distance
+square(x,y) = (x-y)^2
+absval(x,y) = abs(x-y)
+
+# And now a Poisson-sensitive distance.
+# Call this with the 2 sequences, and it returns a closure, a function that
+# measures Poisson-distance for elements of those two particular sequences
+function poissonclosure(seq1, seq2)
+    n1=sum(seq1)
+    n2=sum(seq2)
+    function pdist(x,y)
+        ### WARNING! This does not yet weight properly for Poissonianness
+        ### It only accounts for differences in normalization.
+        (x/n1 - y/n2)^2
+    end
+end
+
+end # module Distance
+
+
+export
+    dtw
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,53 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+
+# Test the Distance functions
+Distance = DynamicTimeWarp.Distance
+@test Distance.square(0,0) == 0
+@test Distance.square(0,1) == 1
+@test Distance.square(9,10) == 1
+@test Distance.square(0,5) == 25
+@test Distance.square(0.0,5.0) == 25.0
+@test Distance.square(0.0,5) == 25.0
+
+@test Distance.absval(0,0) == 0
+@test Distance.absval(0,3) == 3
+@test Distance.absval(0,5) == 5
+@test Distance.absval(-2,1) == 3
+@test Distance.absval(-2,3) == 5
+@test Distance.absval(-2,5) == 7
+@test Distance.absval(-2,-5) == 3
+
+s1, s2 = [1:5], [2:2:10]
+pc = Distance.poissonclosure(s1, s2)
+@test pc(1,2) == 0
+@test pc(5,10) == 0
+@test pc(5,5) == (5/sum(s1)-5/sum(s2))^2
+@test pc(5,0) == (5/sum(s1))^2
+@test pc(0,5) == (5/sum(s2))^2
+
+
+# Test dtw itself
+a=[1,1,1,2,4,6,5,5,5,4,4,3,1,1,1]
+b=[1,1,2,4,6,6,6,5,4,4,4,3,3,3,1]
+cost, match1, match2 = dtw(a,b)
+@test cost==0
+@test match1==[1,2,3,4,5,6,6,6,7,8,9,10,10,11,12,12,12,13,14,15]
+@test match2==[1,1,2,3,4,5,6,7,8,8,8, 9,10,11,12,13,14,15,15,15]
+
+a[end] += 2
+cost, match1, match2 = dtw(a,b)
+@test cost==4
+
+
+a=[1:10]
+b=a+1
+cost, match1, match2 = dtw(a,b)
+@test cost==2
+
+a=zeros(Int,6)
+b=1+a
+cost, match1, match2 = dtw(a,b)
+@test cost==length(a)
