8a50bfd4d87cc24b62476e65942ab8e13266ad1a
Alex Williams <alex.h.willia@gmail.com>
2016-08-07 10:57:48 -0700

    change to gr backend

--- examples/dtw.jl
+++ examples/dtw.jl
@@ -1,21 +1,22 @@
 using DynamicTimeWarp
-using Plots; pyplot()
+using Plots; gr()
 
 function plotdtw(seq1::Vector, seq2::Vector, offset=0.0)
     cost, i1, i2 = dtw(seq1, seq2)
     if offset == 0.0
         offset = 2*(std(seq1) + std(seq2)) + mean(seq1) - mean(seq2)
     end
-    plot(seq1, line=(:red))
-    plot!(seq2+offset, line=(:blue))
+    plot()
     for (a,b) in zip(i1,i2)
         plot!([a,b], [seq1[a], seq2[b]+offset],
              line=(:grey), label=())
     end
     plot!(legend=(:none))
+    plot!(seq1, line=(:red,2))
+    plot!(seq2+offset, line=(:blue,2))
 end
 
 
 seq1 = exp(-(linspace(-3,3,100)).^2)
-seq2 = exp(-(linspace(-3,3,130)).^2)
+seq2 = vcat(zeros(30),seq1)
 plotdtw(seq1,seq2,1.0)
